05e5bf95e8c73110858dd7d900bfa3dfe4bcf0ca
mkdoh3 <mkdoherty3@gmail.com>
2019-05-15 17:36:59 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
 
 
+def my_collect(collection)
+    i = 0
+    modded_collection = []
+    while i < collection.length
+       modded_collection << yield(collection[i])
+       i += 1
+    end
+    modded_collection
+end
